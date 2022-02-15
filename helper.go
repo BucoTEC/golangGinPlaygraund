bb286662184d1201f7860865abb7f2b8c9ec5257
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-02-15 12:26:26 +0100

    fixed helper function

--- helper.go
+++ helper.go
@@ -8,12 +8,15 @@ import (
 	"github.com/joho/godotenv"
 )
 
+//parsing name variable into greet
 func SayHello(name string) string {
 
 	helloThere := fmt.Sprintf("Hello %v, nice to meet you!", name)
 
 	return helloThere
 }
+
+//env fetcher
 func GoDotEnvVariable(key string) string {
 
 	err := godotenv.Load(".env")
